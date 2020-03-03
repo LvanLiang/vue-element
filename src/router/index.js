d8a6973c45efdfe504bc9ce1d3d3dafb7869659f
Liangxp <liangxp0@126.com>
2020-03-03 20:47:54 +0800

    使用emelent-Layout布局，完成路由、嵌套路由功能

--- src/router/index.js
+++ src/router/index.js
@@ -4,11 +4,27 @@ import VueRouter from "vue-router"
 import Main from "../views/Main"
 import Login from "../views/Login"
 
+
+import UserProfile from "../views/user/Profile";
+import UserList from "../views/user/List";
+
 Vue.use(VueRouter);
 
 export default new VueRouter({
   routes: [
-    {path: "/login", name: "Login", component: Login},
-    {path: "/main", name: "Main", component: Main}
+    {
+      path: "/login",
+      name: "Login",
+      component: Login
+    },
+    {
+      path: "/main",
+      name: "Main",
+      component: Main,
+      children: [
+        {path: "/user/profile", component: UserProfile},
+        {path: "/user/list", component: UserList}
+      ]
+    }
   ]
 });
