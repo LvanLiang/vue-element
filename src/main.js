8db29aa761d0315362bd83a4e30f163408173863
liangxp <liangxp0@126.com>
2020-03-04 15:06:12 +0800

    路由钩子与异步请求

--- src/main.js
+++ src/main.js
@@ -1,12 +1,20 @@
-import Vue from 'vue'
-import VueRouter from "vue-router"
+import Vue from 'vue';
+import VueRouter from "vue-router";
 
-import router from "./router/index"
+// 导入自定义路由
+import router from "./router/index";
+
 // 导入 ElementUI
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
-import App from './App'
+// 导入异步通信axios
+import axios from 'axios';
+Vue.prototype.axios = axios;
+
+// 导入vuex
+
+import App from './App';
 
 // 安装路由
 Vue.use(VueRouter);
@@ -17,7 +25,11 @@ Vue.use(ElementUI);
 
 new Vue({
   el: '#app',
-  // 启用路由
+  // 引入组件，使用的是 import App from './App' 定义的 App 组件
+  components: { App },
+  // 模板，会将 index.html 中 <div id="app"></div> 替换为 <App />
+  template: '<App/>',
+  // 启用自定义路由
   router,
   // 启用 ElementUI
   render: h => h(App)
